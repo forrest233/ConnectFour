fc230046b8679f0ae0eaa222aa4dc1a7ce8ce9eb
developer067 <zaidlightning@gmail.com>
2017-12-14 18:17:30 -0500

    Add files via upload

---
+++ connectFour.java
@@ -0,0 +1,57 @@
+/**
+ * Connect Four Java
+ * GUI version of Connect Four
+ * Zaid Omar
+ * Forrest Yuan
+ * Dec. 18, 2017
+ */
+
+import java.io.*; 
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Scanner;
+
+public class connectFour extends JFrame implements ActionListener {
+  
+  public connectFour() // the CONSTRUCTOR
+  {
+    
+    
+    
+    
+    
+  }
+  
+  
+  
+  public void actionPerformed(ActionEvent event)
+  {
+    
+    
+    
+    
+    
+  }
+  
+  public static int [] gameBoard(int [] gameBoardArray) {
+
+    return gameBoardArray;
+  }
+  
+  public static void main(String[] args)throws Exception {
+    
+    Scanner input = new Scanner(System.in);//create scanner
+    
+    System.out.println("Welcome to the Connect Four Game!");
+    
+    
+    
+    
+    
+    new connectFour();
+    
+    input.close();
+  }///end main
+  
+}//end class
